dccde9a454db812fac16b73dd1d09c1f67d8e6fa
Lisovskii Ivan <dnevanek@gmail.com>
2025-03-03 22:13:05 +0300

    deps changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from "tailwindcss";
-
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   css: {
-    postcss: {
-     plugins: [tailwindcss()],
+    preprocessorOptions: {
+      scss: {
+        // Здесь можно добавить глобальные переменные SCSS, если нужно
+      },
     },
-   },
-   server: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
-   }
-
+  },
 })
